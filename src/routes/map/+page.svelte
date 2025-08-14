789f3b9fca0609339f7ce79b26bf71e481a5c4b0
Jeff Allen <jeffallen89@gmail.com>
2025-08-14 08:58:28 -0400

    map updates

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -5,6 +5,30 @@
     
 </script>
 
-<Password/>
+
+<svelte:head>
+
+	<title>Tariffs | School of Cities</title>
+
+	<!-- <meta name="description" content="Repository of design and web components for building data stories, visualizations, maps, and other custom web projects" />
+	<meta name="author" content="Jeff Allen">
+
+	<meta property="og:title" content="Design Components" />
+	<meta property="og:description" content="Repository of design and web components for building data stories, visualizations, maps, and other custom web projects" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://schoolofcities.github.io/design-components/" />
+	<meta property="og:image" content="https://raw.githubusercontent.com/schoolofcities/design-components/main/static/web-card.png" />
+	<meta property="og:locale" content="en_CA">
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Design Components" />
+	<meta name="twitter:description" content="Repository of design and web components for building data stories, visualizations, maps, and other custom web projects" />
+	<meta name="twitter:site" content="https://schoolofcities.github.io/design-components/" />
+	<meta name="twitter:image" content="https://raw.githubusercontent.com/schoolofcities/design-components/main/static/web-card.png" />  -->
+
+</svelte:head>
+
+
+<!-- <Password/> -->
 
 <Map/>
