a9a4e668fbda4f8053ed0eb53245517cf0de0564
an-k45 <an_k27@protonmail.com>
2025-09-18 17:09:50 -0400

    Sort data based on selection

--- src/lib/HorizontalBarChart.svelte
+++ src/lib/HorizontalBarChart.svelte
@@ -5,12 +5,43 @@
     const GRADUATED_COLORS = ["#f1c500", "#fb921f", "#f3603e", "#d73256", "#ab1368"];
     const TARIFF_LIST = ["All goods subject to tariffs", "Automobiles", "Aluminum", "Steel", "Copper", "Lumber", "Energy and natural resources", "Non-CUSMA-Compliant"];
     
+    const TARIFF_NAME_CODES = {
+        "All goods subject to tariffs": 'Total', 
+        "Automobiles": 'Auto', 
+        "Aluminum": 'Alum', 
+        "Steel": 'Steel', 
+        "Copper": 'Cop', 
+        "Lumber": 'Lum', 
+        "Energy and natural resources": 'Ene', 
+        "Non-CUSMA-Compliant": 'CUSMA',
+    }
+    const TARIFF_IMPACT_CODES_PCT = {
+        'Business': "_1",
+        'EmployeeWork': "_2",
+        'EmployeeHome': "_3",
+    }
+    const TARIFF_IMPACT_CODES_COUNT = {
+        'Business': "_B",
+        'EmployeeWork': "_E",
+        'EmployeeHome': "_C",
+    }
+
     let cmaPcts = $state([]);
     let cmaCounts = $state([]);
 
     let metricType = $state("Percent"); // ["Percent", "Count"]
     let impactType = $state("EmployeeHome"); // ["EmployeeHome","EmployeeWork", "Business"] 
     let tariffType = $state("All goods subject to tariffs"); // see full list in TARIFF_LIST
+
+    function sortCmaData(data, metric) {
+        const tariffKey = TARIFF_NAME_CODES[tariffType] + (metric === "Percent" ? TARIFF_IMPACT_CODES_PCT[impactType] : TARIFF_IMPACT_CODES_COUNT[impactType]);
+        return data
+            ?.filter(item => item[tariffKey])
+            .sort(function(a,b) {return b[tariffKey] - a[tariffKey]})
+    }
+
+    let cmaPctsSorted = $derived(sortCmaData(cmaPcts, "Percent"));
+    let cmaCountsSorted = $derived(sortCmaData(cmaCounts, "Count"));
 
     onMount(async () => {
         const response = await fetch('json/cma_tariffs_percents_centroids.json');
@@ -41,81 +72,86 @@
 
     $effect(() => {
         // metricType, impactType, tariffType;
-        console.log(`changed state!! ${metricType}, ${impactType}, ${tariffType}`);
+        // console.log(`changed state!! ${metricType}, ${impactType}, ${tariffType}`);
+        console.log($state.snapshot(cmaPctsSorted));
         
     })
 </script>
 
 <div class="text">
-    <div id = "select-wrapper">
-        <div id="destext">
-            <p style="margin-bottom: -5px;">Select the good/product subject to US tariffs:</p>
+    <div>
+        <div id = "select-wrapper">
+            <div id="destext">
+                <p style="margin-bottom: -5px;">Select the good/product subject to US tariffs:</p>
+            </div>
+            <Select
+                id = 'select'
+                items = {TARIFF_LIST}
+                value = {tariffType}
+                clearable = {false}
+                showChevron = {true}
+                listAutoWidth = {true}
+                searchable = {false}
+                listOffset = {10}
+                on:change = {tariffTypeSelect}
+            />
         </div>
-        <Select
-            id = 'select'
-            items = {TARIFF_LIST}
-            value = {tariffType}
-            clearable = {false}
-            showChevron = {true}
-            listAutoWidth = {true}
-            searchable = {false}
-            listOffset = {10}
-            on:change = {tariffTypeSelect}
-        />
+    
+        <div id="destext">
+        <p style="margin-bottom: -5px;">
+            Select an indicator:
+        </p>
+        </div>
+        <div class="button-group" style="margin-top: 10px;">
+            <button
+                class="toggle-button {impactType === 'Business' ? 'selected' : ''}"
+                type="button"
+                onclick={() => impactTypeSelect("Business")}
+            >
+                Businesses
+            </button>
+            <button
+                class="toggle-button {impactType === 'EmployeeWork' ? 'selected' : ''}"
+                type="button"
+                onclick={() => impactTypeSelect("EmployeeWork")}
+            >
+                Employees (place of work)
+            </button>
+            <button
+                class="toggle-button {impactType === 'EmployeeHome' ? 'selected' : ''}"
+                type="button"
+                onclick={() => impactTypeSelect("EmployeeHome")}
+            >
+                Employees (home)
+            </button>
+        </div>
+    
+        <div id="destext">
+        <p style="margin-bottom: -5px;">
+            Choose how to display this indicator:
+        </p>
+        </div>
+        <div class="button-group">
+            <button
+                class="toggle-button {metricType === 'Percent' ? 'selected' : ''}"
+                type="button"
+                onclick={() => metricSelect("Percent")}
+            >
+                Percent
+            </button>
+            <button
+                class="toggle-button {metricType === 'Count' ? 'selected' : ''}"
+                type="button"
+                onclick={() => metricSelect("Count")}
+            >
+                Total
+            </button>
+        </div>
     </div>
 
-    <div id="destext">
-    <p style="margin-bottom: -5px;">
-        Select an indicator:
-    </p>
-    </div>
-    <div class="button-group" style="margin-top: 10px;">
-        <button
-            class="toggle-button {impactType === 'Business' ? 'selected' : ''}"
-            type="button"
-            onclick={() => impactTypeSelect("Business")}
-        >
-            Businesses
-        </button>
-        <button
-            class="toggle-button {impactType === 'EmployeeWork' ? 'selected' : ''}"
-            type="button"
-            onclick={() => impactTypeSelect("EmployeeWork")}
-        >
-            Employees (place of work)
-        </button>
-        <button
-            class="toggle-button {impactType === 'EmployeeHome' ? 'selected' : ''}"
-            type="button"
-            onclick={() => impactTypeSelect("EmployeeHome")}
-        >
-            Employees (home)
-        </button>
-    </div>
+    <div class='chart-wrapper'>
 
-    <div id="destext">
-    <p style="margin-bottom: -5px;">
-        Choose how to display this indicator:
-    </p>
     </div>
-    <div class="button-group">
-        <button
-            class="toggle-button {metricType === 'Percent' ? 'selected' : ''}"
-            type="button"
-            onclick={() => metricSelect("Percent")}
-        >
-            Percent
-        </button>
-        <button
-            class="toggle-button {metricType === 'Count' ? 'selected' : ''}"
-            type="button"
-            onclick={() => metricSelect("Count")}
-        >
-            Total
-        </button>
-    </div>
-
-    <!-- Chart markup will go here -->
 </div>
 
 <style>
